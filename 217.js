ee9a4e484cc55f4a69274dedb595f065467e1077
Avi Dalal <avi413@gmail.com>
2022-08-24 11:35:39 +0300

    242

--- 217.js
+++ 217.js
@@ -6,14 +6,17 @@
  * Given an integer array nums, 
  * return true if any value appears at least twice in the array, 
  * and return false if every element is distinct.
+ * time O(n)
+ * space O(n)
  */
 var containsDuplicate = function(nums) {
+    //space O(n)
     const set = new Set();
-    //O(n)
+    //time O(n)
     for( let i = 0 ; i < nums.length ; i++) {
-        //O(1)
+        //time O(1)
         if(set.has(nums[i])) return true;
-        //O(1)
+        //time O(1)
         set.add(nums[i]); 
     }
     return false;
